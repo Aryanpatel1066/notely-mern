cdec55fc6964e4ac0cbf9ccdb67d86cf183d56ed
aryanPatel <aryanpatel1248@gmail.com>
2025-02-11 12:29:17 +0530

    done the db configs

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -1,4 +1,5 @@
-module.exports={
-    db_name:"todoApp",
-    db_url:"mongodb://localhost/todoApp"
+module.exports = {
+    db_name: "todoApp",
+    db_url: "mongodb+srv://aryanpatel1248:frRd0cCzkv1pS2pl@cluster0.i2ouh.mongodb.net/todoApp?retryWrites=true&w=majority"
 }
+//    db_url:"mongodb://localhost/todoApp"
