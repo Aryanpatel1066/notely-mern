ddb720f8cc87fcaabf9d3149ad2e5ebaf692e01f
aryanPatel <aryanpatel1248@gmail.com>
2025-02-11 17:53:17 +0530

    add url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS configuration with explicit settings
 const corsOptions = {
-    origin: "http://localhost:5173",  
+    origin: "https://notely-mernb.onrender.com/",  
     methods: ["GET", "POST", "PUT", "DELETE"],   
     allowedHeaders: ["Content-Type", "Authorization"],  
 };
