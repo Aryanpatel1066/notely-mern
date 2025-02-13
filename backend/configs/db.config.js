fbad3e3fef32446629f5d16a672901e2a9938ef9
aryanPatel <aryanpatel1248@gmail.com>
2025-02-13 19:10:52 +0530

    wrong

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -1,5 +1,4 @@
 module.exports = {
     db_name: "todoApp",
-    db_url: 'mongodb+srv://aryandev1066:jC98HrcyFt01M82M@cluster0.prdlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    db_url: 'mongodb+srv://aryandev1066:newPassword123@cluster0.prdlr.mongodb.net/todoApp?retryWrites=true&w=majority&appName=Cluster0'
 }
-//    db_url:"mongodb://localhost/todoApp"
